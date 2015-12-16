58c062e4aff2514c850421f4c18b346714fd3aa9
Patrick Hutchinson <rphutchinson@gmail.com>
2015-12-16 08:27:56 -0500

    day 6

---
+++ day6/part2.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const rl = require('readline').createInterface({
+  input: require('fs').createReadStream('input.txt')
+});
+
+
+//initialize the array
+let state = [];
+for(let i=0; i<1000; i++){
+  state[i] = [];
+  for(let j=0; j<1000; j++){
+    state[i][j] = 0;
+  }
+};
+
+rl.on('line', function (instruction) {
+  _processInstruction(instruction.replace('turn ', '').split(' '));
+});
+
+rl.on('close', function(){
+  console.log(_totalBrightness());  
+});
+
+function _processInstruction(instruction){
+  let action = instruction[0], 
+    startCoords = instruction[1].split(','),
+    endCoords = instruction[3].split(',');
+
+  for(let x = Number(startCoords[0]); x <= Number(endCoords[0]); x++){
+    for(let y = Number(startCoords[1]); y <= Number(endCoords[1]); y++){
+      
+      if(action === 'on') state[x][y]++;
+      if(action === 'off') state[x][y] = state[x][y] && state[x][y] - 1;
+      if(action === 'toggle') state[x][y] = state[x][y] + 2;
+    }
+  }
+}
+
+
+function _totalBrightness(){
+  let total = 0;
+  for(let i=0; i<state.length; i++){
+    for(let j=0; j<state[i].length; j++){
+      total += state[i][j];
+    }
+  }
+
+  return total;
+}
